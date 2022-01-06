1d0901682cca31b14e670f7e0290625fb992c4f0
PAGF188 <pablo.garcia.fernandez.at@gmail.com>
2022-01-06 01:13:08 +0100

    elm listo

--- Entregar/3-ann/3-ann.py
+++ Entregar/3-ann/3-ann.py
@@ -3,11 +3,15 @@ Practica 3: Exercises of ANN classifiers
 Autor: Pablo García Fernández.
 
 """
-
-import numpy as np
-from sklearn.neighbors import *
-from sklearn.metrics import *
+from ELM import Elm
 from crea_folds import crea_folds
+import numpy as np
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import *
+from sklearn.model_selection import *
+import warnings
+warnings.filterwarnings("ignore")
+import pdb
 
 def pr3(dataset_name, dataset_train=None, dataset_test=None):
     """ Función que ejecuta los 4 ejercicios mencionados
@@ -24,36 +28,140 @@ def pr3(dataset_name, dataset_train=None, dataset_test=None):
     # EJERCICIO 4 -----------------------------------------------------------------
     # 
     if dataset_name == 'LBP' or dataset_name == 'Coocur':
-        print(f"EJERCICIO 5 (LDA sobre {dataset_name} dataset)")
-        print("─────────────────────────────────────────")
-
-        # Cargar datos
-        data_train = np.loadtxt(dataset_train)
-        y_train = data_train[:,-1]
-        x_train = data_train[:,0:-1]
-
-        data_test = np.loadtxt(dataset_test)
-        y_test = data_test[:,-1]
-        x_test = data_test[:,0:-1]
-
+        pass
     # EJERCICIO 2,3 (sobre wine y hepatitis)
     else:
-        # EJERCICIO 2 -------------------------------------------------------------
-        # Calcular acc., kappa y cm usando LDA y todo el dataset
-        print("EJERCICIO 2 (LDA whole dataset):")
-        
-        
-        print(f"acc. (con sklearn): {acc:.2f}%")  # mismo resultado
-        print("─────────────────────────────────────────\n\n\n")
+        #ej2(dataset_name)
+        ej3(dataset_name)
 
-        # EJERCICIO 3 -------------------------------------------------------------
-        # Calcular acc., kappa y cm usando MLP y ELM con cross validation.
-        print("EJERCICIO 2 (LDA whole dataset):")
-        
-        
-        print(f"acc. (con sklearn): {acc:.2f}%")  # mismo resultado
-        print("─────────────────────────────────────────\n\n\n")
 
+def ej3(dataset_name):
+    """
+    EJERCICIO 3:
+    Calcular acc., kappa y cm usando MLP y ELM + cross validation + sintonizacion.
+    """
+    #####################################################################################3
+    # Ejercicio 3_1 -> MLP 
+    print("EJERCICIO 3_1 (MLP cross validation):")
+
+    datos = np.loadtxt('../data/' + dataset_name)
+    y=datos[:,0]-1; x=np.delete(datos,0,1)
+    C = len(np.unique(y))
+
+    K=4
+    [tx,ty,vx,vy,sx,sy] = crea_folds(x,y,K)
+
+    #preprocesamiento
+    for k in range(K):
+        med=np.mean(tx[k],0); dev=np.std(tx[k],0)
+        tx[k]=(tx[k]-med)/dev
+        vx[k]=(vx[k]-med)/dev
+        sx[k]=(sx[k]-med)/dev
+    
+    #sintonizacion
+    print("Sintonizacion:")
+    H=3
+    IK=30
+    vkappa=np.zeros(K); kappa_mellor=-np.Inf
+    neurons_mellor = []
+    for i in range(1,H+1):
+        print('%10s '%('H%i'%i),end='')
+    print('%10s'%('Kappa(%)'))
+
+    for h in range(1,H+1):
+        for j in range(10,IK+1,10):
+            neurons=[C]
+            for m in range(1,h+1):
+                neurons.insert(0,j)
+                for k in range(K):
+                    modelo = MLPClassifier(hidden_layer_sizes=neurons).fit(tx[k],ty[k])
+                    z=modelo.predict(vx[k])
+                    vkappa[k]=cohen_kappa_score(vy[k],z)
+            kappa_med=np.mean(vkappa)
+            for i in range(h):
+                print('%10i'%(neurons[i]),end='')
+            for i in range(h+1, H+1):
+                print('%10s'%'',end='')
+            print('%10.1f'%(100*kappa_med))
+            if kappa_med>kappa_mellor:
+                kappa_mellor=kappa_med
+                neurons_mellor = neurons
+    print(f"Mejor arquitectura: {neurons_mellor[: -1]}, kappa={100*kappa_mellor:.2f}%\n")
+
+    #test
+    print("Test:")
+    v_accuracy = np.zeros(K)
+    mc = np.zeros([C,C])
+    for k in range(K):
+        tx[k]=np.vstack((tx[k],vx[k]))
+        ty[k]=np.concatenate((ty[k],vy[k]))
+        modelo = MLPClassifier(hidden_layer_sizes=neurons_mellor).fit(tx[k],ty[k])
+        z=modelo.predict(sx[k]); y=sy[k]
+        vkappa[k]=100*cohen_kappa_score(y,z)
+        v_accuracy[k] = 100*accuracy_score(y,z)
+        mc+=confusion_matrix(y,z)
+
+    kappa=np.mean(vkappa); mc/=K; accuracy = np.mean(v_accuracy)
+    print(f"acc.: {accuracy:.2f}%\nkappa: {kappa:.2f}%\ncf = \n{mc}")
+    print("─────────────────────────────────────────\n\n\n")
+
+
+
+    #####################################################################################3
+    # Ejercicio 3_2 -> ELM 
+    print("EJERCICIO 3_2 (ELM cross validation):")
+    datos = np.loadtxt('../data/' + dataset_name)
+    y=datos[:,0]-1; x=np.delete(datos,0,1)
+    C = len(np.unique(y))
+
+    K=4
+    [tx,ty,vx,vy,sx,sy] = crea_folds(x,y,K)
+
+    #preprocesamiento
+    for k in range(K):
+        med=np.mean(tx[k],0); dev=np.std(tx[k],0)
+        tx[k]=(tx[k]-med)/dev
+        vx[k]=(vx[k]-med)/dev
+        sx[k]=(sx[k]-med)/dev
+
+    #sintonizacion
+    print("Sintonizacion:")
+    IK=40
+    vkappa=np.zeros(K); kappa_mellor=-np.Inf
+    neurons_mellor = 5
+    print('%10s %10s'%('IK','Kappa(%)'))
+
+    for i in range(5,IK+1,5):
+        for k in range(K):
+            modelo = Elm(hidden_units=i, x=tx[k], y=ty[k])
+            _, _ = modelo.fit()
+            z=modelo.predict(vx[k])
+            vkappa[k]=cohen_kappa_score(vy[k],z)
+
+        kappa_med=np.mean(vkappa)
+        print('%10i %10.1f'%(i,100*kappa_med))
+        if kappa_med>kappa_mellor:
+            kappa_mellor = kappa_med
+            neurons_mellor = i
+    print(f"Mejor nº neuronas ocultas: {neurons_mellor}, kappa={100*kappa_mellor:.2f}%\n")
+
+    #test
+    print("Test:")
+    v_accuracy = np.zeros(K)
+    mc = np.zeros([C,C])
+    for k in range(K):
+        tx[k]=np.vstack((tx[k],vx[k]))
+        ty[k]=np.concatenate((ty[k],vy[k]))
+        modelo = Elm(hidden_units=neurons_mellor, x=tx[k], y=ty[k])
+        _, _ = modelo.fit()
+        z=modelo.predict(sx[k]); y=sy[k]
+        vkappa[k]=100*cohen_kappa_score(y,z)
+        v_accuracy[k] = 100*accuracy_score(y,z)
+        mc+=confusion_matrix(y,z)
+
+    kappa=np.mean(vkappa); mc/=K; accuracy = np.mean(v_accuracy)
+    print(f"acc.: {accuracy:.2f}%\nkappa: {kappa:.2f}%\ncf = \n{mc}")
+    print("─────────────────────────────────────────\n\n\n")
 
 
 # Ejecutamos
